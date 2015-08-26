607cd2763f5ee13ba0ba31e780b3349ba7d36d4d
Christopher Hiller <boneskull@boneskull.com>
2015-08-25 23:22:19 -0700

    reformat & cleanup index.js

--- index.js
+++ index.js
@@ -1,56 +1,53 @@
 'use strict';
+
 var gutil = require('gulp-util');
 var map = require('map-stream');
 var spawn = require('win-spawn');
 var dargs = require('dargs');
 
-module.exports = function (options) {
-	options = options || {};
-	var passedArgs = dargs(options, ['bundleExec']);
-	var bundleExec = options.bundleExec;
+module.exports = function jekyll(options) {
+  var passedArgs = dargs(options || {}, ['bundleExec']);
+  var jekyllArgs = (options.bundleExec ? ['bundle', 'exec'] : [])
+    .concat('jekyll', 'build')
+    .concat(passedArgs);
 
-	return map(function (file, cb) {
+  return map(function spawnJekyll(file, cb) {
+    var errors = '';
+    var proc;
 
-		if (file.isStream()) {
-			return cb(new gutil.PluginError('gulp-jekyll', 'Streaming not supported'));
-		}
+    if (file.isStream()) {
+      return cb(new gutil.PluginError('gulp-jekyll',
+        'Streaming not supported'));
+    }
 
-		var args = [
-			'jekyll',
-			'build'
-		].concat(passedArgs);
+    proc = spawn(jekyllArgs.shift(), jekyllArgs)
+      .on('error', function onError(err) {
+        return cb(new gutil.PluginError('gulp-jekyll', err));
+      })
+      .on('close', function onClose(code) {
+        if (code === 127) {
+          return cb(new gutil.PluginError('gulp-jekyll',
+            'You need to have Ruby and Jekyll installed and in your PATH for ' +
+            'this task to work. See http://jekyllrb.com/docs/installation/'));
+        }
 
-		if (bundleExec) {
-			args.unshift('bundle', 'exec');
-		}
+        if (errors) {
+          return cb(new gutil.PluginError('gulp-jekyll',
+            '\n' +
+            errors.replace('Use --trace for backtrace.\n', '')));
+        }
 
-		var cp = spawn(args.shift(), args);
+        if (code > 0) {
+          return cb(new gutil.PluginError('gulp-jekyll',
+            'Exited with error code ' + code));
+        }
 
-		cp.on('error', function (err) {
-			return cb(new gutil.PluginError('gulp-jekyll', err));
-		});
+        cb(null, file);
+      });
 
-		var errors = '';
-		cp.stderr.setEncoding('utf8');
-		cp.stderr.on('data', function (data) {
-			errors += data;
-		});
-
-		cp.on('close', function (code) {
-			if (code === 127) {
-				return cb(new gutil.PluginError('gulp-jekyll', 'You need to have Ruby and Jekyll installed and in your PATH for this task to work.'));
-			}
-
-			if (errors) {
-				return cb(new gutil.PluginError('gulp-jekyll', '\n' + errors.replace('Use --trace for backtrace.\n', '')));
-			}
-
-			if (code > 0) {
-				return cb(new gutil.PluginError('gulp-jekyll', 'Exited with error code ' + code));
-			}
-
-			cb(null, file);
-
-		});
-	});
+    proc.stderr.setEncoding('utf8');
+    proc.stderr.on('data', function onData(data) {
+      errors += data;
+    });
+  });
 };
