179eae89b04519d12cedd1b85266e8993f75124c
Christopher Hiller <boneskull@boneskull.com>
2015-08-29 23:07:35 -0700

    fix prepublish

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -2,12 +2,16 @@
 
 var which = require('which');
 
-if (!which.sync('ruby')) {
+try {
+  which.sync('ruby');
+} catch (ignored) {
   return console.log('WARNING: "ruby" executable not found in PATH!  Ruby is ' +
     'required.');
 }
 
-if (!which.sync('jekyll')) {
+try {
+  which.sync('jekyll');
+} catch (ignored) {
   return console.log('WARNING: "jekyll" executable not found in PATH!  ' +
     'Jekyll is required.  Try "gem install jekyll"');
 }
